e83791424cb432b902a2b277282fa91253df4853
tarek421 <tarekmahmud740@gmail.com>
2023-04-09 00:12:13 +0600

    end

--- src/components/FlashSaleProducts/Card.js
+++ src/components/FlashSaleProducts/Card.js
@@ -9,8 +9,17 @@ const Card = ({ item }) => {
             <div className="card-body">
                 <h6 className='catagory'>{catagory}</h6>
                 <h4 className='card-title mb-0'>{title}</h4>
-                <Rating initialValue={rating} size={15} />
-                <h6 className='price'>${newPrice} <span className='old-price'><del>${oldPrice}</del></span></h6>
+                <div className='hover-none'>
+                    <Rating initialValue={rating} size={15} />
+                    <h6 className='price'>${newPrice} <span className='old-price'><del>${oldPrice}</del></span></h6>
+                </div>
+                <div className='hover-block'>
+                    <div className="d-flex justify-content-between">
+                        <img src="https://img.icons8.com/ios-glyphs/30/null/visible--v1.png" alt='eye' />
+                        <img src="https://img.icons8.com/ios-glyphs/30/null/hearts.png" alt='love' />
+                        <img src="https://img.icons8.com/material-outlined/24/null/sorting-arrows.png" alt='arrow' />
+                    </div>
+                </div>
                 <a href="/" class="btn w-100">Add to cart <i class="fa-regular fa-bag-shopping"></i></a>
             </div>
         </div>
